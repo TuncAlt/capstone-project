263175e8cce2d09938bb8ada9fa4f37815c20075
Tuncer Altun <tunc.alt@gmail.com>
2023-03-09 14:38:21 +0100

    Add DeviceOverview, use use-local-storage-hook, devices are now displayed and user gets information about the device

--- components/DeviceList/deviceList.js
+++ components/DeviceList/deviceList.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaExclamationTriangle } from "react-icons/fa";
 
 export default function DeviceList({ devices }) {
   return (
@@ -6,7 +7,29 @@ export default function DeviceList({ devices }) {
       <h1>Devices</h1>
       <ul>
         {devices?.map((device, index) => (
-          <li key={index}>{device.name}</li>
+          <li key={index}>
+            {device.name}{" "}
+            {device.readings && device.readings.length > 0 ? (
+              <>
+                {device.readings[1].temperature}°C
+                {Number(device.readings[1].temperature) >
+                Number(device.maxTemp) ? (
+                  <span style={{ color: "red" }}>
+                    {" "}
+                    <FaExclamationTriangle />
+                  </span>
+                ) : Number(device.readings[1].temperature) <
+                  Number(device.minTemp) ? (
+                  <span style={{ color: "red" }}>
+                    {" "}
+                    <FaExclamationTriangle />
+                  </span>
+                ) : null}
+              </>
+            ) : (
+              "please log temperature"
+            )}
+          </li>
         ))}
       </ul>
     </>
