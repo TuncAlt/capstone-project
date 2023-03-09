616418b320d9323e873e0727692b2bb2b178aaf1
Tuncer Altun <tunc.alt@gmail.com>
2023-03-09 14:46:05 +0100

    Delet modified files

--- components/UseLocalStorage/UseLocalStorageDevices.js
+++ components/UseLocalStorage/UseLocalStorageDevices.js
@@ -1,10 +1,18 @@
-import useLocalStorageState from "use-local-storage-state";
+import { useState, useEffect } from "react";
 
 export default function useLocalStorageDevices() {
-  const [devices, setDevices] = useLocalStorageState("devices", []);
+  const [devices, setDevices] = useState([]);
+
+  useEffect(() => {
+    const storedDevices = JSON.parse(localStorage.getItem("devices"));
+    if (storedDevices) {
+      setDevices(storedDevices);
+    }
+  }, []);
 
   const addDevice = (device) => {
     setDevices([...devices, device]);
+    localStorage.setItem("devices", JSON.stringify([...devices, device]));
   };
 
   return [devices, addDevice];
