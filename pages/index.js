e1c8965f473fe64d8ad407ab440fe4302952f303
Tuncer Altun <tunc.alt@gmail.com>
2023-03-13 12:52:11 +0100

    delete unnecessary import

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,4 @@
 import DeviceList from "@/components/DeviceList/deviceList";
-import styled from "styled-components";
-
-const StyledButton = styled.button`
-  border: solid 1px;
-  padding: 10px;
-  background-color: #072a5e;
-  color: white;
-  border-radius: 16px;
-  cursor: pointer;
-  width: 80%;
-`;
-const StyledButtonContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  margin-top: 10px;
-`;
 
 export default function Home({ devices }) {
   return (
