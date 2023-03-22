9d14941740b547e49ce28f5aa4a4c15379c5ec7c
Tuncer Altun <tunc.alt@gmail.com>
2023-03-22 14:51:12 +0100

    wip

--- components/EditComponents/DeviceEdit.js
+++ components/EditComponents/DeviceEdit.js
@@ -1,6 +1,6 @@
 import AddDeviceForm from "../Forms/AddDeviceForm/AddDeviceForm";
 
-export default function DeviceEdit() {
+export default function DeviceEdit({ devices }) {
   // find the device with matching deviceId
   const device = devices?.find((device) => device.id === deviceId);
   // if device not found, render null
