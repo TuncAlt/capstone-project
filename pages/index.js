9cee8df8219edb25e7650ee3214d0f7636444c2d
Tuncer Altun <tunc.alt@gmail.com>
2023-03-30 14:58:05 +0200

    fix vercel deployment issue

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import DeviceList from "@/components/DeviceList/DeviceList";
+import DeviceList from "../components/DeviceList/deviceList";
 
 export default function Home({ devices }) {
   return <DeviceList devices={devices} />;
